b9c5cc17d86a26702062b6b13e21be93b5890486
veerviren <virenvariya7@gmail.com>
2023-01-26 15:48:39 +0530

    calender added

--- habit_tracker/lib/screens/home_page.dart
+++ habit_tracker/lib/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:habit_tracker/screens/habit_selector.dart';
 import 'package:habit_tracker/contants.dart';
 import 'package:habit_tracker/widgets/profile_pic_name.dart';
+import 'package:habit_tracker/widgets/calendar.dart';
+import 'package:horizontal_calendar/horizontal_calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,7 +22,21 @@ class _HomePageState extends State<HomePage> {
             padding: const EdgeInsets.all(8.0),
             child: profilePicandName(),
           ),
+          Calender(),
         ]),
+      ),
+    );
+  }
+
+  HorizontalCalendar Calender() {
+    return HorizontalCalendar(
+      date: DateTime.now(),
+      textColor: Colors.black45,
+      backgroundColor: Colors.white,
+      selectedColor: Colors.blue,
+      showMonth: true,
+      onDateSelected: (date) => print(
+        date.toString(),
       ),
     );
   }
